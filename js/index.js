d6bf6ff721d9a62876de2e8c0d6bbc837cbce630
dclkent1parker <dcl.quentind@18pixel.fr>
2018-11-20 09:49:04 +0100

    add hover

--- js/index.js
+++ js/index.js
@@ -8,3 +8,14 @@ $(".slider").mousewheel(function(event, delta, deltaX, deltaY) {
 				     }
 						
 	});
+var activated= false;
+$('#burgerButton').click(function(){
+	if(!activated){
+	$('#hamburger').css('display', 'block');
+	$('#burgerButton').css('background', 'white');
+	activated=true;}
+	else{$('#hamburger').css('display', 'none');
+	$('#burgerButton').css('background', 'black');
+	activated=false;
+	}
+})
