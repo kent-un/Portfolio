2975a90bba94326f299ddb3da81068aab048ae31
dclkent1parker <dcl.quentind@18pixel.fr>
2018-11-20 16:46:18 +0100

    add comp

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
-$(".slider").mousewheel(function(event, delta, deltaX, deltaY) {
-		var o = '';
-		if (deltaY > 0){
-		                  $(".slider").animate({scrollLeft : '-=200'}, 'slow');
-                                  }
-		else if (deltaY < 0){
-                          $(".slider").animate({scrollLeft :'+=200'}, 'slow');
-				     }
-						
-	});
+
 var activated= false;
+$('#burgerButton').on('tap',function(){
+	if(!activated){
+	$('#hamburger').css('display', 'block');
+	$('#burgerButton').css('background', 'white');
+	activated=true;}
+	else{$('#hamburger').css('display', 'none');
+	$('#burgerButton').css('background', 'black');
+	activated=false;
+	}
+})
 $('#burgerButton').click(function(){
 	if(!activated){
 	$('#hamburger').css('display', 'block');
@@ -18,4 +19,28 @@ $('#burgerButton').click(function(){
 	$('#burgerButton').css('background', 'black');
 	activated=false;
 	}
+})
+$('#prog').click(function(){
+		$('#hoverProg').css('display', 'flex');
+		$('.modalHover').click(function(){
+			$('#hoverProg').css('display', 'none');
+		})
+})
+$('#proj').click(function(){
+	$('#hoverProj').css('display', 'flex');
+	$('.modalHover').click(function(){
+		$('#hoverProj').css('display', 'none');
+	})
+})
+$('#log').click(function(){
+	$('#hoverLog').css('display', 'flex');
+	$('.modalHover').click(function(){
+		$('#hoverLog').css('display', 'none');
+	})
+})
+$('#com').click(function(){
+	$('#hoverCom').css('display', 'flex');
+	$('.modalHover').click(function(){
+		$('#hoverCom').css('display', 'none');
+	})
 })
